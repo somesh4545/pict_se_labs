2afd48632f910600072d44d1220a7e69560ed377
Somesh Somani <57597556+somesh4545@users.noreply.github.com>
2022-12-28 18:56:12 +0530

    Update assign7_b.cpp

--- dsa/assign7_b.cpp
+++ dsa/assign7_b.cpp
@@ -1,64 +1,65 @@
-// Prim's algorithm
-
 #include <iostream>
-#include <limits.h>
 using namespace std;
 
 #define V 5
 
-void findPrims(int g[V][V]){
-    int visited[V];
-    for(int i=0; i<V; i++){
-        visited[i] = 0;
-    }
-    visited[0] = 1;
+int find(int p[], int x)
+{
+    if (p[x] == x)
+        return x;
+    return find(p, p[x]);
+}
 
-    int no_edge = 0;
+void makeUnion(int p[], int x, int y)
+{
+    int a = find(p, x);
+    int b = find(p, y);
+    if (a == b)
+        return;
+    p[b] = a;
+}
 
-    while(no_edge != V-1){
+void kruskals(int graph[V][V])
+{
+    int parent[V];
+    for (int i = 0; i < V; i++)
+        parent[i] = i;
+
+    int no_edges = 0;
+    while (no_edges < V - 1)
+    {
         int min = INT_MAX;
-        int x = 0;
-        int y = 0;
-
-        for(int i=0; i<V; i++){
-            if(visited[i]){
-                for(int j=0; j<V; j++){
-                    if(visited[j]==0 && g[i][j] > 0){
-                        if(min > g[i][j]){
-                            min = g[i][j];
-                            x = i;
-                            y = j;
-                        }
-                    }
+        int x = 0, y = 0;
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if ((find(parent, i) != find(parent, j)) && graph[i][j] != 0 && graph[i][j] < min)
+                {
+                    min = graph[i][j];
+                    x = i;
+                    y = j;
                 }
             }
         }
-        cout << "Edge no: " << no_edge << " between " <<  x << "-" << y << " " << " cost= " << min << endl;
-        visited[y] = 1;
-        no_edge++;
+        no_edges++;
+        makeUnion(parent, x, y);
+        cout << x << "-" << y << " cost: " << min << endl;
     }
 }
 
-int main(){
+int main()
+{
 
-//     int graph[V][V] = {
-//       { INT_MAX, 12, INT_MAX, 25, INT_MAX },
-//       { 12, INT_MAX, 11, 8, 12 },
-//       { INT_MAX, 11, INT_MAX, INT_MAX, 17 },
-//       { 25, 8, INT_MAX, INT_MAX, 15 },
-//       { INT_MAX, 12, 17, 15, INT_MAX },
-//    };
+    int graph[V][V] = {
+        {0, 0, 3, 0, 0},
+        {0, 0, 10, 4, 0},
+        {3, 10, 0, 2, 6},
+        {0, 4, 2, 0, 1},
+        {0, 0, 6, 1, 0},
+    };
 
-    int graph[V][V];
-
-    cout << "Enter graph's nodes value 5-5: " << endl;
-    for(int i=0; i<V; i++){
-        for(int j=0; j<V; j++){
-            cin >> graph[i][j];
-        }
-    }
-
-    findPrims(graph);
+    kruskals(graph);
 
     return 0;
 }
