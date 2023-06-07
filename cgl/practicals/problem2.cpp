b6c7220f09b2f11b95ee82b02cff7f63a6d2f510
Somesh Somani <57597556+somesh4545@users.noreply.github.com>
2023-06-07 11:25:19 +0530

    added shearing x and y

--- cgl/practicals/problem2.cpp
+++ cgl/practicals/problem2.cpp
@@ -1,117 +1,4 @@
-#include <iostream>
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
 
-#include <math.h>
-
-using namespace std;
-
-double et[3][3] = {1}, ans[3][3]={0};
-float xi, yi;
-int length;
-
-void dda(int x1, int y1, int x2, int y2){
-    float dx, dy, incx, incy, steps;
-    dx = x2 - x1;
-    dy = y2 - y1;
-    steps = max(abs(dx), abs(dy));
-    incx = dx!=0 ? steps / dx : 0;
-    incy = dy!=0 ? steps / dy : 0;
-    int x=x1, y=y1;
-    glColor3f(1, 0, 0);
-    glBegin(GL_POINTS);
-    for(int i=1; i<=steps; i++){
-        glVertex2i(x, y);
-        x += incx;
-        y += incy;
-    }
-    glEnd();
-    glFlush();
-}
-
-void drawET(double triangle[3][3]){
-    glBegin(GL_LINE_LOOP);
-    for(int i=0; i<3; i++){
-        glVertex2i(triangle[i][0], triangle[i][1]);
-    }
-    glEnd();
-    glFlush();
-}
-
-void multi3x3(double mat1[3][3], double mat2[3][3]){
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            int sum = 0;
-            for(int k=0; k<3; k++){
-                sum += mat1[i][k] * mat2[k][j];
-            }
-            ans[i][j] = sum;
-        }
-    }
-}
-
-void translation(){
-    int tx, ty;
-    cout << "Enter the sx: ";
-    cin >> tx;
-    cout << "Enter the sy: ";
-    cin >> ty;
-
-    double tMatrix[3][3]={0};
-    tMatrix[0][0] = 1;
-    tMatrix[1][1] = 1;
-    tMatrix[2][2] = 1;
-    tMatrix[2][0] = tx;
-    tMatrix[2][1] = ty;
-
-    multi3x3(et, tMatrix);
-    glColor3f(0, 0, 1);
-    drawET(ans);
-}
-
-void scaling(){
-    float sx, sy;
-    cout << "Enter scaling for x: ";
-    cin >> sx;
-    cout << "Enter scaling for y: ";
-    cin >> sy;
-    double Smatrix[3][3]={0};
-    Smatrix[0][0] = sx;
-    Smatrix[1][1] = sy;
-    Smatrix[2][2] = 1;
-
-    multi3x3(et, Smatrix);
-    glColor3f(1, 1, 0);
-    drawET(ans);
-}
-
-void rotation(){
-    double rx, ry, angle;
-    cout << "Enter rotation angle: ";
-    cin >> angle;
-    cout << "Enter arbiratary x point: ";
-    cin >> rx;
-    cout << "Enter arbitratiry y point: ";
-    cin >> ry;
-
-    double rMatrix[3][3] = {0};
-    rMatrix[0][0] = cos(angle);
-    rMatrix[0][1] = sin(angle);
-    rMatrix[1][0] = -sin(angle);
-    rMatrix[1][1] = cos(angle);
-    rMatrix[2][0] = rx - (rx*cos(angle)) + (ry*sin(angle));
-    rMatrix[2][1] = ry - (rx*sin(angle)) - (ry*cos(angle));
-    rMatrix[2][2] = 1;
-
-    glColor3f(1, 0, 1);
-    multi3x3(et, rMatrix);
-    drawET(ans);
-}
-
-void menu(int item){
     switch(item){
     case 1:
         translation();
@@ -121,6 +8,12 @@ void menu(int item){
         break;
     case 3:
         rotation();
+        break;
+    case 4:
+        shearingX();
+        break;
+    case 5:
+        shearY();
         break;
     default:
         break;
@@ -177,6 +70,8 @@ int main(int argc, char** argv){
     glutAddMenuEntry("Translation", 1);
     glutAddMenuEntry("Scaling", 2);
     glutAddMenuEntry("Rotation", 3);
+    glutAddMenuEntry("Shearing x", 4);
+    glutAddMenuEntry("Shearing y", 5);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 
     glutMainLoop();
